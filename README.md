ca3001e146eb7e90c035b2a68cfea2856d09e628
shivssm <shivssm95@gmail.com>
2024-01-26 10:50:30 +0530

    Update Backend URL

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Final project for ZTM course
 
 
+Render Backend URL(Note: don't use mine):-https://smartbrain-glnj.onrender.com
--- src/App.js
+++ src/App.js
@@ -69,9 +69,8 @@ class App extends Component {
 
     app.models.predict('face-detection', this.state.input)
       .then(response => {
-        console.log('hi', response)
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://smartbrainapi-re5w.onrender.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
